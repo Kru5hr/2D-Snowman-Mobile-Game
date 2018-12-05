d0c933811d6c63952718d24d1b53e57b5fab04cd
Jake Taylor <31117134+Kru5hr@users.noreply.github.com>
2018-12-05 20:36:37 +0000

    Project upload

---
+++ 2D Snowman Mobile Game/Assets/Scripts/Menu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+	private void onLevelSelect(int currentIndex)
+    {
+        SceneManager.LoadScene("Lvl_Selector");
+        Debug.Log("Selecting level : " + currentIndex);
+    }
+}
