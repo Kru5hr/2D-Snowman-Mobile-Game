90d8ac13aa08f9e141a5b564650148fb80a27a80
Jake Taylor <31117134+Kru5hr@users.noreply.github.com>
2018-12-15 17:38:51 +0000

    swipe script

---
+++ 2D Snowman Mobile Game/Assets/Scripts/Swipe.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swipe : MonoBehaviour
+{
+    private bool tap, swipeLeft, swipeRight;
+    private bool isDragging = false;
+    private Vector2 startTouch, swipeDelta;
+
+    private void Update()
+    {
+        tap = swipeLeft = swipeRight = false;
+
+        #region Standalone Inputs
+        if (Input.GetMouseButtonDown(0))
+        {
+            isDragging = true; 
+            startTouch = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            tap = true;
+            isDragging = false;
+            Reset();
+        }
+        #endregion
+
+        #region Mobile Inputs
+        if(Input.touches.Length > 0)
+        {
+            if (Input.touches[0].phase == TouchPhase.Began)
+            {
+                tap = true;
+                isDragging = true;
+                startTouch = Input.touches[0].position;
+            }
+            else if (Input.touches[0].phase == TouchPhase.Ended || Input.touches[0].phase == TouchPhase.Canceled)
+            {
+                isDragging = false;
+                Reset();
+            }
+        }
+        #endregion
+
+        //Calculate the distance
+        swipeDelta = Vector2.zero;
+        if (isDragging)
+        {
+            if (Input.touches.Length > 0)
+                swipeDelta = Input.touches[0].position - startTouch;
+            else if(Input.GetMouseButton(0))
+                swipeDelta = (Vector2)Input.mousePosition - startTouch;
+        }
+
+        //Did we cross the deadzone
+        if (swipeDelta.magnitude > 125)
+        {
+            //Which direction?
+            float x = swipeDelta.x;
+            float y = swipeDelta.y;
+            if (Mathf.Abs(x) > Mathf.Abs(y))
+            {
+                //left or right
+                if (x < 0)
+                    swipeLeft = true;
+                else
+                    swipeRight = true;
+            }
+        }
+    }
+
+    private void Reset()
+    {
+        startTouch = swipeDelta = Vector2.zero;
+        isDragging = false;
+    }
+
+    public Vector2 SwipeDelta { get { return swipeDelta; } }
+    public bool SwipeLeft { get { return swipeLeft;  } }
+    public bool SwipeRight { get { return swipeRight; } }
+}
---
+++ 2D Snowman Mobile Game/Assets/Scripts/SwipeTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeTest : MonoBehaviour
+{
+    public Swipe swipeControls;
+    public Transform player;
+    private Vector3 desiredPostion;
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+        if (swipeControls.SwipeLeft)
+            desiredPostion += Vector3.left;
+        if (swipeControls.SwipeRight)
+            desiredPostion += Vector3.right;
+
+        player.transform.position = Vector3.MoveTowards(player.transform.position, desiredPostion, 3f * Time.deltaTime);
+    }
+}
