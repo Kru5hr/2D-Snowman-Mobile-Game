6eee55059358f046e2dd277c66bbdd433de5f30e
Jake Taylor <31117134+Kru5hr@users.noreply.github.com>
2018-12-23 12:02:33 +0000

    pause menu

---
+++ 2D Snowman Mobile Game/Assets/Scripts/Pause.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+    bool isPaused = false;
+
+    public void pauseGame()
+    {
+        if (isPaused)
+        {
+            Time.timeScale = 1f;
+            isPaused = false;
+        }
+        else
+        {
+            Time.timeScale = 0f;
+            isPaused = true;
+            Debug.Log("Game Paused");
+        }
+    }
+}
---
+++ 2D Snowman Mobile Game/Assets/Scripts/TouchSwipe.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchSwipe : MonoBehaviour
+{
+    private Vector2 startTouchPosition, endTouchPosition;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            startTouchPosition = Input.GetTouch(0).position;
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            endTouchPosition = Input.GetTouch(0).position;
+
+            if ((endTouchPosition.x < startTouchPosition.x) && transform.position.x > -1.75f)
+                transform.position = new Vector2(transform.position.x - 1.75f, transform.position.y);
+
+            if ((endTouchPosition.x > startTouchPosition.x) && transform.position.x < 1.75f)
+                transform.position = new Vector2(transform.position.x + 1.75f, transform.position.y);
+        }
+    }
+}
