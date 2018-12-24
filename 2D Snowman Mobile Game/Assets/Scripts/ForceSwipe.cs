544a2373194b51a4f28cfd693676db3c76a41301
Jake Taylor <31117134+Kru5hr@users.noreply.github.com>
2018-12-24 10:41:45 +0000

    smooth sliding player controls

---
+++ 2D Snowman Mobile Game/Assets/Scripts/ForceSwipe.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceSwipe : MonoBehaviour
+{
+    Vector2 startPos, endPos, direction; //touch start position, touch end position, swipe direction
+    float touchTimeStart, touchTimeFinish, timeInterval; //calculate swipe time
+
+    [Range(0.05f, 1f)]               //slide for inspector window
+    public float throwForce = 0.03f; //to control force throw
+    
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) //once touch input started
+        {
+            touchTimeStart = Time.time;
+            startPos = Input.GetTouch(0).position;
+        }
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            touchTimeFinish = Time.time;
+
+            timeInterval = touchTimeFinish - touchTimeStart;
+
+            endPos = Input.GetTouch(0).position;
+
+            direction = startPos - endPos;
+
+            GetComponent<Rigidbody2D>().AddForce(-direction / timeInterval * throwForce);
+        }
+    }
+}
--- 2D Snowman Mobile Game/Assets/Scripts/SmoothMoveSwipe.cs
+++ 2D Snowman Mobile Game/Assets/Scripts/SmoothMoveSwipe.cs
@@ -12,22 +12,17 @@ public class SmoothMoveSwipe : MonoBehaviour {
     // Update is called once per frame
     private void Update()
     {
-        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
-        pos.x = Mathf.Clamp01(pos.x);
-        pos.y = Mathf.Clamp01(pos.y);
-        transform.position = Camera.main.ViewportToWorldPoint(pos);
-
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) //once touch input started
             startTouchPosition = Input.GetTouch(0).position;
 
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
         {
             endTouchPosition = Input.GetTouch(0).position;
 
-            if ((endTouchPosition.x < startTouchPosition.x) && transform.position.x > -1.75f)
+            if ((endTouchPosition.x < startTouchPosition.x) && transform.position.x > -1.5f)
                 StartCoroutine(Fly("left"));
 
-            if ((endTouchPosition.x > startTouchPosition.x) && transform.position.x < 1.75f)
+            if ((endTouchPosition.x > startTouchPosition.x) && transform.position.x < 1.5f)
                 StartCoroutine(Fly("right"));
         }
     }
@@ -40,7 +35,7 @@ public class SmoothMoveSwipe : MonoBehaviour {
                 flyTime = 0f;
                 startRocketPosition = transform.position;
                 endRocketPosition = new Vector3
-                    (startRocketPosition.x - 1.75f, transform.position.y, transform.position.z);
+                    (startRocketPosition.x - 1.5f, transform.position.y, transform.position.z);
 
                 while (flyTime < flightDuration)
                 {
@@ -55,7 +50,7 @@ public class SmoothMoveSwipe : MonoBehaviour {
                 flyTime = 0f;
                 startRocketPosition = transform.position;
                 endRocketPosition = new Vector3
-                    (startRocketPosition.x + 1.75f, transform.position.y, transform.position.z);
+                    (startRocketPosition.x + 1.5f, transform.position.y, transform.position.z);
 
                 while (flyTime < flightDuration)
                 {
