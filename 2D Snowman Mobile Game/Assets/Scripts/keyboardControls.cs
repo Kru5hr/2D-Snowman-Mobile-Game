c7d4cbf7489ec1b35c68ff32ba46ce3c4fd0d45e
Jake Taylor <31117134+Kru5hr@users.noreply.github.com>
2019-01-17 21:06:49 +0000

    added player keyboard controls

---
+++ 2D Snowman Mobile Game/Assets/Scripts/keyboardControls.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keyboardControls : MonoBehaviour
+{
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (Input.GetKey(KeyCode.A))
+            rb.AddForce(Vector3.left);
+        if (Input.GetKey(KeyCode.D))
+            rb.AddForce(Vector3.right);
+    }
+}
