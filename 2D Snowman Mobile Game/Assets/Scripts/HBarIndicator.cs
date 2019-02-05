870131c03627a0f5c5ce9f0a231028cb209e2f3b
Jake Taylor <31117134+Kru5hr@users.noreply.github.com>
2019-02-05 19:11:28 +0000

    damage indicator text  pop-up

---
+++ 2D Snowman Mobile Game/Assets/Scripts/HBarIndicator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HBarIndicator : MonoBehaviour
+{
+    public float moveSpeed;
+    public int damageNumber;
+    public TMP_Text displayNumber;
+    public float timeToDestroy;
+	
+    // Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        displayNumber.text = "" + damageNumber;
+        transform.position = new Vector3(transform.position.x, transform.position.y + (moveSpeed * Time.deltaTime), transform.position.z);
+	}
+}
--- 2D Snowman Mobile Game/Assets/Scripts/snowflakeScore.cs
+++ 2D Snowman Mobile Game/Assets/Scripts/snowflakeScore.cs
@@ -5,19 +5,24 @@ using UnityEngine.UI;
 
 public class snowflakeScore : MonoBehaviour
 {
+    public int damageToGive;
+    public Transform hitPoint;
+    public GameObject hText;
+
     public void OnParticleCollision(GameObject col)
     {
 
         if (col.gameObject.name == "ground")
         {
             HealthBar.health -= 10f;
-            //Destroy(gameObject);
+
+            var clone = (GameObject)Instantiate(hText, hitPoint.position, Quaternion.Euler(Vector3.zero));
+            clone.GetComponent<HBarIndicator>().damageNumber = damageToGive;
         }
         else if (col.gameObject.name == "player_snowman")
         {
             tmpScore.scoreAmount += 1;
             HealthBar.health += 10f;
-            //Destroy(gameObject);
         }
     }
 }
