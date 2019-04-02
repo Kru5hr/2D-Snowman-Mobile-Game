7708e56f93a3d30f4f81ff796ca7e74237b6ab99
Jake Taylor <31117134+Kru5hr@users.noreply.github.com>
2019-04-02 16:13:20 +0100

    changed controls script
    still need to find a way of only letting player swipe on x axis and tap to jump on y axis

---
+++ 2D Snowman Mobile Game/Assets/Scripts/Controls_dup.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Controls_dup : MonoBehaviour
+{
+    Vector2 startPos, endPos, direction; //touch start position, touch end position, swipe direction
+    float touchTimeStart, touchTimeFinish, timeInterval; //calculate swipe time
+    public float jumpForce;
+    public bool jumpAllowed = false;
+    public bool isGrounded;
+    public Rigidbody2D rb;
+    public AudioSource source1;
+    public AudioSource source2;
+    public AudioSource source3;
+    [Range(0.05f, 1f)]               //slide for inspector window
+    public float throwForce = 0.03f; //to control force throw
+
+    private Touch touch;
+    
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        JumpIfAllowed();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //*************TOUCH CONTROLS*********************
+        //SLIDE
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    touchTimeStart = Time.time;
+                    startPos = Input.GetTouch(0).position;
+                    break;
+
+                case TouchPhase.Ended:
+
+                    touchTimeFinish = Time.time;
+                    timeInterval = touchTimeFinish - touchTimeStart;
+                    endPos = Input.GetTouch(0).position;
+                    direction = startPos - endPos;
+                    rb.AddForce(-direction / timeInterval * throwForce);
+
+                    if (isGrounded == true)
+            {
+                        jumpAllowed = true;
+                    }
+
+                    break;
+            }
+        }
+
+        if (Input.touchCount == 1 && startPos == endPos && isGrounded == true)
+        {
+            jumpAllowed = true;
+        }
+
+        //********KEYBOARD CONTROLS****************
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            source2.Play();
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector3.left);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            source3.Play();
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        {
+            jumpAllowed = true;
+        }
+
+        JumpIfAllowed();
+
+    }
+
+    void JumpIfAllowed()
+    {
+        if (jumpAllowed)
+        {
+            rb.AddForce(Vector2.up * jumpForce);
+            jumpAllowed = false;
+            isGrounded = false;
+            source1.Play();
+            //HealthBar.health -= 10f;
+            Debug.Log(HealthBar.health);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+}
--- 2D Snowman Mobile Game/Assets/Scripts/snowflakeScore.cs
+++ 2D Snowman Mobile Game/Assets/Scripts/snowflakeScore.cs
@@ -15,9 +15,15 @@ public class SnowflakeScore : MonoBehaviour
     ParticleSystem pSystem;
     public float emValue1 = 0.5f;
     public float emValue2 = 0.75f;
-    public float emValue3 = 1f;
+    public float emValue3 = 1.25f;
     public float emValue4 = 2f;
-    public float emValue5 = 5f;
+    public float emValue5 = 3f;
+
+    public float spValue1 = 1f;
+    public float spValue2 = 1.5f;
+    public float spValue3 = 2f;
+    public float spValue4 = 2.5f;
+    public float spValue5 = 3f;
 
     public void Start()
     {
@@ -25,8 +31,10 @@ public class SnowflakeScore : MonoBehaviour
         pSystem = GetComponent<ParticleSystem>();
 
         var em = pSystem.emission;
+        var sp = pSystem.velocityOverLifetime;
 
         em.rateOverTime = emValue1;
+        sp.speedModifier = spValue1;
     }
 
     public void OnParticleCollision(GameObject col)
@@ -44,7 +52,7 @@ public class SnowflakeScore : MonoBehaviour
         else if (col.gameObject.tag == "Player")
         {
             TmpScore.scoreAmount += 1;
-            HealthBar.health += 10f;
+            HealthBar.health += 5f;
             Debug.Log(HealthBar.health);
             soundAudio.clip = ting;
             soundAudio.Play();
@@ -54,25 +62,30 @@ public class SnowflakeScore : MonoBehaviour
     public void Update()
     {
         var em = pSystem.emission;
+        var sp = pSystem.velocityOverLifetime;
 
         if (TmpScore.scoreAmount == 5)
         {
             em.rateOverTime = emValue2;
+            sp.speedModifier = spValue2;
         }
 
         if (TmpScore.scoreAmount == 10)
         {
             em.rateOverTime = emValue3;
+            sp.speedModifier = spValue3;
         }
 
         if (TmpScore.scoreAmount == 25)
         {
             em.rateOverTime = emValue4;
+            sp.speedModifier = spValue4;
         }
 
         if (TmpScore.scoreAmount == 50)
         {
             em.rateOverTime = emValue5;
+            sp.speedModifier = spValue5;
         }
     }
 }
